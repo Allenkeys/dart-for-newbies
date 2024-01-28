a248d9123a646175a35a859ff32f1bfb5024cf2e
Joshua <79511167+Allenkeys@users.noreply.github.com>
2024-01-28 22:23:37 +0100

    Test integers in dart

--- 003_numbers/main.dart
+++ 003_numbers/main.dart
@@ -1,1 +1,14 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  int? int1 = null;
+  int int2 = 200;
+
+  num num1 = 34;
+  num num2 = 34.5;
+
+  double dob1 = 10.3;
+  double doubSum = int1 == null ? int2 / num1 : num1 + dob1;
+  print(doubSum.runtimeType);
+  print(doubSum);
+  print(num1 + num2);
+  print((num1 + num2).runtimeType);
+}
