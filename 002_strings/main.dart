503fed6a2084d94c57cb8eeb5ef65e5b582a2fca
Joshua <79511167+Allenkeys@users.noreply.github.com>
2024-01-28 21:51:14 +0100

    Try string, string methods and properties

--- 002_strings/main.dart
+++ 002_strings/main.dart
@@ -1,1 +1,11 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+    String name = 'Josh';
+    String address = '''
+        1 JC Eze Avenue, Github Nigeria
+    ''';
+    String sentence = 'My name is ' + name + ' and I live at '+address;//Concatenation
+    String sentence2 = 'My name is $name and I live at \n $address'; // Interpolation
+    print(sentence);
+    print(sentence2.toUpperCase());
+    print(sentence.length);
+}
