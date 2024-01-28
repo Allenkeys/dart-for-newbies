5a82025e444e46be6d1cb2d92bb4140e6ab565dd
Joshua <79511167+Allenkeys@users.noreply.github.com>
2024-01-28 22:44:12 +0100

    Dealing with fixed length list

--- 004_lists/main.dart
+++ 004_lists/main.dart
@@ -1,1 +1,6 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  /// Fixed length list
+  List<int> fixedIntList = List.filled(2, 1, growable: false);
+  fixedIntList[1] = 2;
+  print(fixedIntList);
+}
